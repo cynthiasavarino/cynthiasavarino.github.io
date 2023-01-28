ccd76556ef026f6d940dd7cc84689841d97dde31
cynthiasavarino <123840879+cynthiasavarino@users.noreply.github.com>
2023-01-28 20:30:29 -0300

    Add files via upload

---
+++ CVterminado.js
@@ -0,0 +1,40 @@
+document.getElementById("b1").onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("b1").innerHTML="Roberta Ramos";
+   }
+
+document.getElementById("b2").onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("b2").innerHTML="2/04/1993";
+   }
+
+document.getElementById("b3").onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("b3").innerHTML="5595 W 6th St";
+   }
+
+document.getElementById("b4").onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("b4").innerHTML="cel:(964) 694-7166";
+   }
+
+document.getElementById("b5").onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("b5").innerHTML="INSTITUTO PRIVADO SL - PERIODO: 2013-2014";
+   }
+
+document.getElementById("b6").onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("b6").innerHTML="UCCUYO - PERIODO: 2015-2018";
+   }
+
+document.getElementById("b7").onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById("b7").innerHTML="UNSL - PERIODO: 2019-2022";
+   }
+
+
+document.getElementById("b8").onclick= function () {
+    console.log ("captura el evento click");
+    document.getElementById ("DATOS").style.display = "block";
+}
